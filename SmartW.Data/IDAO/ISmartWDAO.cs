5fc90ed8c115ee544b6dc7104d4d77dc86812892
Alyzande Renard <alyzande@gmail.com>
2019-05-06 23:03:23 +0100

    Add project files.

---
+++ SmartW.Data/IDAO/ISmartWDAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartW.Data.IDAO
+{
+    public interface ISmartWDAO
+    {
+        //IList<SmartW.Data.Garment> GetGarment();
+
+        IList<GarmentWear> GetGarmentWear(string Garment);
+
+        IList<Garment> GetUserGarment(string id);
+
+    }
+
+
+}
